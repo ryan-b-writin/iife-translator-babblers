655618c6ec233a4f8e220265bab9d7d51bcc8be9
Ryan B <ryan.bliton@gmail.com>
2016-02-25 16:26:31 -0600

    added comments to code to help explain

--- japanese.js
+++ japanese.js
@@ -1,4 +1,7 @@
+
+
 var Babbler = (function(babbler){
+  // Lexicon object. replace with your own lexicon!
   var japanese = {
     merry: "merii",
     christmas: "kurisumasu",
@@ -7,18 +10,29 @@ var Babbler = (function(babbler){
     new: "omeditou",
     year: "gozaimasu",
   };
-  
+
+// this function accepts a string and outputs a different string.
   babbler.translateToJapanese = function(string) {
+
+    // split sentence up into an array so the words are indexed and searchable with "indexOf"
     var array = string.split(" ")
+
+    //use .indexOf to find specific words in the array and overwrite them with Japanese replacements
     array[array.indexOf("merry")] = japanese.merry;
     array[array.indexOf("christmas")] = japanese.christmas;
     array[array.indexOf("and")] = japanese.and;
     array[array.indexOf("happy")] = japanese.happy;
     array[array.indexOf("new")] = japanese.new;
     array[array.indexOf("year")] = japanese.year;
+
+    //make the array back into a string
     var sentence = array.join(" ");
+
+    //return new translated sentence
     return sentence;
   }
   return babbler;
+
+//pass in Babbler object to augment, OR if this is the first Babbler, pass in an empty object to start
 }(Babbler || {}));
 
