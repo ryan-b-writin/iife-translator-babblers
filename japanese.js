cd0b1a866cd54e498212c187b92f34d0deb10e69
Ryan B <ryan.bliton@gmail.com>
2016-02-25 18:08:09 -0600

    cleaned up the code and added more comments

--- japanese.js
+++ japanese.js
@@ -3,30 +3,31 @@
 var Babbler = (function(babbler){
   // Lexicon object. replace with your own lexicon!
   var japanese = {
-    merry: "merii",
+    merry:     "merii",
     christmas: "kurisumasu",
-    and: "soshite",
-    happy: "akemashite",
-    new: "omeditou",
-    year: "gozaimasu",
+    and:       "soshite",
+    happy:     "akemashite",
+    new:       "omeditou",
+    year:      "gozaimasu",
   };
 
 // this function accepts a string and outputs a different string.
-  babbler.translateToJapanese = function(string) {
+  babbler.translateToJapanese = function(string) { 
 
     // split sentence up into an array so the words are indexed and searchable with "indexOf"
-    var array = string.split(" ")
+    var array = string.split(" ") // array = ["merry", "christmas", "and", "happy", "new", "year"]
+
 
     //use .indexOf to find specific words in the array and overwrite them with Japanese replacements
-    array[array.indexOf("merry")] = japanese.merry;
-    array[array.indexOf("christmas")] = japanese.christmas;
-    array[array.indexOf("and")] = japanese.and;
-    array[array.indexOf("happy")] = japanese.happy;
-    array[array.indexOf("new")] = japanese.new;
-    array[array.indexOf("year")] = japanese.year;
+    array[array.indexOf("merry")]     = japanese.merry; // array[0] = "merii"
+    array[array.indexOf("christmas")] = japanese.christmas; // array[1] = "kurisumasu"
+    array[array.indexOf("and")]       = japanese.and;
+    array[array.indexOf("happy")]     = japanese.happy;
+    array[array.indexOf("new")]       = japanese.new;
+    array[array.indexOf("year")]      = japanese.year;
 
     //make the array back into a string
-    var sentence = array.join(" ");
+    var sentence = array.join(" "); 
 
     //return new translated sentence
     return sentence;
