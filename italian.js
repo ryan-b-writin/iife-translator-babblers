06f7697922f03f1c573f135bc65b229501e13ea1
Ryan B <ryan.bliton@gmail.com>
2016-02-27 11:50:05 -0600

    worked on italian babbler object

--- italian.js
+++ italian.js
@@ -1,33 +1,31 @@
 // lexicon object: Italian
-var babbler = function(babbler) {
-  var italian  = {
+var babbler = (function(babbler) {
+  var italian = {
     merry:"bon",  
-    Christmas:"Natale", 
-    Happy:"Felice", 
-    New:"Nuovo", 
-    Year:"Anno",
+    christmas:"Natale", 
+    happy:"Felice", 
+    new:"Nuovo", 
+    year:"Anno",
     holiday:"vacanza", 
     and:"e"
-}; 
+  }; 
 
-// function to accept a string from user and     return the new translation
+// function to accept a string from user and   return the new translation
   babbler.translateToItalian = function(string) {
 //the original array must be split up and searchable to find the translated word
-    var array = string.split("")
-
+    var array = string.split(" ")
 // index.Of is used to find the English word and replace it with the Italian 
-    array[array.indexOf("merry")] = Italian.merry;
-    array[array.indexOf("christmas")] = Italian.christmas;
-    array[array.indexOf("happy")] = Italian.happy;
-    array[array.indexOf("new")] = Italian.new;
-    array[array.indexOf("year")] = Italian.year;
-    array[array.indexOf("holiday")] = Italian.holiday;
-    array[array.indexOf("and")] = Italian.and;
-  }
+    array[array.indexOf("merry")] = italian.merry;
+    array[array.indexOf("christmas")] = italian.christmas;
+    array[array.indexOf("happy")] = italian.happy;
+    array[array.indexOf("new")] = italian.new;
+    array[array.indexOf("year")] = italian.year;
+    array[array.indexOf("holiday")] = italian.holiday;
+    array[array.indexOf("and")] = italian.and;
 
-    var sentence = array.join("");
+    var sentence = array.join(" ");
     return sentence; 
-  }
+}
 return babbler;
 
 // pass the babbler into the object ??? also, where is the missing curly bracket? 
